2309f569ebc9f756cc3c1a782180011ff38d338b
hatsumaru41 <81270310+hatsumaru41@users.noreply.github.com>
2022-10-23 21:12:20 -0500

    Categoria: Controller/Service/ServiceImpl/Repository

--- src/main/java/com/pictomanis/controllers/CategoriaController.java
+++ src/main/java/com/pictomanis/controllers/CategoriaController.java
@@ -5,6 +5,7 @@ import com.pictomanis.serviceinterfaces.ICategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -14,8 +15,30 @@ CategoriaController {
     @Autowired
     private ICategoriaService cService;
     @PostMapping
-    public void registrar(@RequestBody Categoria c) {cService.insert(c);}
+    public void registrar(@RequestBody Categoria c) {
+        cService.insert(c);
+    }
     @GetMapping
-    public List<Categoria> list() {return cService.list();}
+    public List<Categoria> list() {
+        return cService.list();
+    }
+
+    @PutMapping
+    public void update(@RequestBody Categoria c){
+        cService.insert(c);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        cService.delete(id);
+    }
+
+    @PostMapping("/find")
+    public List<Categoria> findName(@RequestBody Categoria cs) throws ParseException{
+        List<Categoria> listCategoria;
+        cs.setNameCategoria(cs.getNameCategoria());
+        listCategoria = cService.findName(cs.getNameCategoria());
+        return listCategoria;
+    }
 
 }
